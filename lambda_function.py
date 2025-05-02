11034ef095e2fc44dd6d2c3ab02bd116861e1315
Rushith Reddy Gajjala <venkatrushithreddy@gmail.com>
2025-05-03 05:09:47 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,8 @@
+import os
+
+def lambda_handler(event, context):
+    greeting = os.environ.get('GREETING_MESSAGE', 'Hello, World!')
+    return {
+        'statusCode': 200,
+        'body': greeting
+    }
